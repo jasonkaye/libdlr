6a03da886f012f5d8e54b3cd6c4103e78acf4e70
Jason Kaye <jkaye@flatironinstitute.org>
2021-05-29 16:58:19 -0400

    Separated some pieces of dlr_dyson_mf into subroutines for modularity

--- src/dlr_dyson.f90
+++ src/dlr_dyson.f90
@@ -65,7 +65,7 @@
 
       allocate(g0(rank),g0mat(rank,rank))
 
-      call g0init(beta,rank,dlrit,it2cf,it2cfpiv,phi,mu,g0,g0mat)
+      call g0init_it(beta,rank,dlrit,it2cf,it2cfpiv,phi,mu,g0,g0mat)
 
 
       ! --- Fixed point iteration for G ---
@@ -73,7 +73,9 @@
       ! Either use input initial guess, or use G0 as initial guess
 
       if (useg0==1) then
+
         g = g0
+
       endif
 
       allocate(sig(rank),gnew(rank))
@@ -132,9 +134,9 @@
       ! phi       - tensor taking a set of DLR coefficients to matrix of
       !               convolution by corresponding DLR expansion
       ! g0        - DLR coefficients of g0, produced by subroutine
-      !               g0init
+      !               g0init_it
       ! g0mat     - matrix of convolution by G0, produced by subroutine
-      !               g0init
+      !               g0init_it
       ! sig       - values of self-energy Sigma at DLR imaginary time
       !               nodes.
       !
@@ -186,7 +188,8 @@
       end subroutine dyson_it_lin
 
 
-      subroutine g0init(beta,rank,dlrit,it2cf,it2cfpiv,phi,mu,g0,g0mat)
+      subroutine g0init_it(beta,rank,dlrit,it2cf,it2cfpiv,phi,mu,g0,&
+          g0mat)
 
       ! Get DLR coefficient representation of G0, and matrix of
       ! convolution by G0, for use in Dyson solver
@@ -232,7 +235,7 @@
 
       call dlr_convmat(rank,phi,it2cf,it2cfpiv,g0,g0mat)
 
-      end subroutine g0init
+      end subroutine g0init_it
 
 
 
@@ -285,15 +288,15 @@
 
       implicit none
       integer rank,numit,useg0,it2cfpiv(rank),mf2cfpiv(rank),dlrmf(rank)
+      integer info
       real *8 beta,dlrit(rank),mu,it2cf(rank,rank)
       real *8 cf2it(rank,rank),g(rank),w,fptol
       complex *16 mf2cf(rank,rank),cf2mf(rank,rank)
 
-      integer i,j,info
+      integer i
       real *8 one
-      real *8, allocatable :: sig(:),gnewr(:)
-      complex *16, allocatable :: g0(:),gmf(:),sigmf(:),gnew(:)
-      complex *16, external :: kfunf_mf
+      real *8, allocatable :: sig(:),gnew(:)
+      complex *16, allocatable :: g0(:)
 
       one = 1.0d0
 
@@ -301,28 +304,20 @@
 
       allocate(g0(rank))
 
-      do i=1,rank
-
-        g0(i) = -kfunf_mf(dlrmf(i),beta*mu)
-
-      enddo
+      call g0init_mf(beta,rank,dlrmf,mu,g0)
 
 
       ! --- Fixed point iteration for G ---
 
       ! Either use input initial guess, or use G0 as initial guess
 
-      allocate(gmf(rank))
-
       if (useg0==1) then
         
-        gmf = g0
-
-        call dlr_mfexpnd(rank,mf2cf,mf2cfpiv,gmf,g)
+        call dlr_mfexpnd(rank,mf2cf,mf2cfpiv,g0,g)
 
       endif
 
-      allocate(sig(rank),gnew(rank),gnewr(rank),sigmf(rank))
+      allocate(sig(rank),gnew(rank))
 
       do i=1,numit
 
@@ -331,27 +326,16 @@
 
         call sigeval(rank,g,sig)
 
-        ! DLR coefficient representation of Sigma
+        ! Solve linear Dyson equation
 
-        call dlr_expnd(rank,it2cf,it2cfpiv,sig,sig)
-
-        ! Matsubara frequency representation of Sigma
-
-        sigmf = matmul(cf2mf,sig)
-
-        ! Invert linear Dyson equation
-
-        gnew = g0/(one-beta**2*g0*sigmf)
-
-        ! DLR coefficient representation of solution
-
-        call dlr_mfexpnd(rank,mf2cf,mf2cfpiv,gnew,gnewr)
+        call dyson_mf_lin(beta,rank,it2cf,it2cfpiv,mf2cf,mf2cfpiv,&
+          cf2mf,g0,sig,gnew)
 
         ! Check self-consistency
 
-        if (maxval(abs(matmul(cf2it,gnewr-g)))<fptol) then
+        if (maxval(abs(matmul(cf2it,gnew-g)))<fptol) then
 
-          g = gnewr
+          g = gnew
           numit = i
           info = 0
 
@@ -359,7 +343,7 @@
 
         else
 
-          g = w*gnewr + (one-w)*g
+          g = w*gnew + (one-w)*g
 
         endif
       enddo
@@ -367,6 +351,104 @@
       info = -1
 
       end subroutine dlr_dyson_mf
+
+
+      subroutine g0init_mf(beta,rank,dlrmf,mu,g0)
+
+      ! Get G0 on Matsubara frequency DLR grid, for use in Dyson solver
+      !
+      ! Input:
+      !
+      ! beta      - inverse temperature
+      ! rank      - rank of DLR (# basis functions)
+      ! dlrit     - selected imaginary time nodes (tau points)
+      ! dlrmf     - selected Matsubara frequency nodes
+      ! mu        - single particle energy parameter
+      !
+      ! Output:
+      !
+      ! g0        - G0 on Matsubara frequency DLR grid
+
+      integer rank,dlrmf(rank)
+      real *8 beta,mu
+      complex *16 g0(rank)
+
+      integer i
+      complex *16, external :: kfunf_mf
+
+      ! Evaluate G0 on Matsubara frequency DLR grid 
+
+      do i=1,rank
+
+        g0(i) = -kfunf_mf(dlrmf(i),beta*mu)
+
+      enddo
+
+      end subroutine g0init_mf
+
+
+      subroutine dyson_mf_lin(beta,rank,it2cf,it2cfpiv,mf2cf,mf2cfpiv,&
+          cf2mf,g0,sig,g)
+
+      ! Solve the linear Dyson equation, with given self-energy, using
+      ! DLR in imaginary time
+      !
+      ! Input:
+      !
+      ! beta      - inverse temperature
+      ! rank      - rank of DLR (# basis functions)
+      ! it2cf     - imaginary time grid values -> DLR coefficients
+      !               transform matrix in lapack LU storage format
+      ! it2cfpiv  - pivot matrix for it2cf in lapack LU storage
+      !               format
+      ! mf2cf     - Matsubara frequency grid values -> DLR coefficients
+      !               transform matrix in lapack LU storage format
+      ! mf2cfpiv  - pivot matrix for mf2cf in lapack LU storage format
+      ! cf2mf     - DLR coefficients -> Matsubara frequency grid values
+      ! g0        - Values of G0 on Matsubara frequency DLR grid,
+      !               produced by subroutine g0init_mf
+      ! sig       - values of self-energy Sigma at DLR imaginary time
+      !               nodes.
+      !
+      ! Output:
+      !
+      ! g         - DLR coefficients of solution of linear Dyson
+      !               equation 
+
+      implicit none
+      integer rank,it2cfpiv(rank),mf2cfpiv(rank)
+      real *8 beta,it2cf(rank,rank)
+      real *8 sig(rank),g(rank)
+      complex *16 mf2cf(rank,rank),cf2mf(rank,rank),g0(rank)
+
+      integer j,info1
+      integer, allocatable :: ipiv(:)
+      real *8 one
+      real *8, allocatable :: sigc(:)
+      complex *16, allocatable :: sigmf(:),gmf(:)
+
+      one = 1.0d0
+
+      allocate(sigc(rank),gmf(rank))
+
+      ! DLR coefficient representation of Sigma
+
+      call dlr_expnd(rank,it2cf,it2cfpiv,sig,sigc)
+
+      ! Matsubara frequency representation of Sigma
+
+      sigmf = matmul(cf2mf,sigc)
+
+      ! Invert linear Dyson equation
+
+      gmf = g0/(one-beta**2*g0*sigmf)
+
+      ! DLR coefficient representation of solution
+
+      call dlr_mfexpnd(rank,mf2cf,mf2cfpiv,gmf,g)
+
+
+      end subroutine dyson_mf_lin
 
 
 
