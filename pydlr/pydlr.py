5f7abe51411961bd32f4158cd4983d3cd34ed4e0
Hugo U. R. Strand <hugo.strand@gmail.com>
2021-08-27 09:22:26 +0200

    [doc] pydlr imtime methods

--- pydlr/pydlr.py
+++ pydlr/pydlr.py
@@ -44,6 +44,7 @@ class dlr(object):
     
     def __init__(self, lamb, eps=1e-15, xi=-1,
                  max_rank=500, nmax=None, verbose=False, python_impl=True):        
+
         self.xi = np.sign(xi)
         self.lamb = lamb
         self.eps = eps
@@ -78,37 +79,128 @@ class dlr(object):
     # -- Imaginary time
 
     def get_tau_over_beta(self):
-        """Get grid in scaled imaginary time :math:`\\tau/\\beta \\in [0, 1]`"""
-        return self.dlrit
+        
+        """Get grid in scaled imaginary time :math:`\\tau/\\beta \\in [0, 1]`
+
+        Returns
+        -------
+
+        ttau_l : (n) ndarray
+            Scaled imaginary time points :math:`\\tilde{\\tau}_l = \\tau_l / \\beta` 
+            for the DLR representation, :math:`\\tilde{\\tau}_l \\in [0, 1]`.
+        """
+        
+        ttau_l = self.dlrit
+        return ttau_l
 
     
     def get_tau(self, beta):
-        """Get :math:`\\tau`-grid in imaginary time :math:`\\tau \\in [0, \\beta]`."""
+
+        """Get :math:`\\tau`-grid in imaginary time :math:`\\tau \\in [0, \\beta]`.
+
+        Parameters
+        ----------
+
+        beta : float
+            Inverse temperature :math:`\\beta`
+
+        Returns
+        -------
+
+        tau_l : (n) ndarray
+            Imaginary time points :math:`\\tau_l` for the DLR representation, :math:`\\tau_l \\in [0, \\beta]`.
+        """
+        
         tau_l = self.get_tau_over_beta() * beta
         return tau_l
 
 
     def dlr_from_tau(self, G_laa):
-        """Transform the rank-3 array_like Green's function `G_laa` from imaginary time to DLR space."""
+
+        """Transform the rank-3 array_like Green's function `G_laa` from imaginary time to DLR space.
+
+        Parameters
+        ----------
+
+        G_laa : (n,m,m), array_like
+            Green's function in imaginary time with :math:`m \\times m` orbital indices.
+
+        Returns
+        -------
+
+        G_xaa : (n,m,m), ndarray
+            Green's function i DLR coefficient space with :math:`m \\times m` orbital indices.
+        """
+
         G_xaa = lu_solve((self.dlrit2cf, self.it2cfpiv), G_laa)
         return G_xaa
 
 
     def tau_from_dlr(self, G_xaa):
-        """Transform the rank-3 array_like Green's function `G_xaa` from DLR space to imaginary time."""
+
+        """Transform the rank-3 array_like Green's function `G_xaa` from DLR space to imaginary time.
+
+        Parameters
+        ----------
+
+        G_xaa : (n,m,m), array_like
+            Green's function in DLR coefficient space with :math:`m \\times m` orbital indices.
+
+        Returns
+        -------
+
+        G_laa : (n,m,m), ndarray
+            Green's function in imaginary time with :math:`m \\times m` orbital indices.
+        """
+
         G_laa = np.tensordot(self.T_lx, G_xaa, axes=(1, 0))
         return G_laa
 
 
     def tau_from_legendre(self, G_naa):
-        """Transform the rank-3 array_like Green's function `G_naa` from Legendre coefficient space to imaginary time."""
+
+        """Transform the rank-3 array_like Green's function `G_naa` from Legendre coefficient space to imaginary time.
+
+        Parameters
+        ----------
+
+        G_naa : (n,m,m), array_like
+            Green's function in Legendre coefficient space with :math:`m \\times m` orbital indices.
+
+        Returns
+        -------
+
+        G_laa : (n,m,m), ndarray
+            Green's function in imaginary time with :math:`m \\times m` orbital indices.
+        """
+
         x = 2 * self.get_tau_over_beta() - 1
         G_laa = np.rollaxis(leg.legval(x, G_naa), -1)
         return G_laa
 
 
-    def eval_dlr_tau(self, G_xaa, tau, beta):
-        """Evaluate the DLR coefficient Green's function `G_xaa` at arbibrary points in imaginary time."""
+    def eval_dlr_tau(self, G_xaa, tau_k, beta):
+
+        """Evaluate the DLR coefficient Green's function `G_xaa` at arbibrary points in imaginary time.
+
+        Parameters
+        ----------
+
+        G_xaa : (n,m,m), array_like
+            Green's function in DLR coefficient space with :math:`m \\times m` orbital indices.
+
+        tau_k : (k), array_like
+            Imaginary time points :math:`\\tau_k` where to evaluate the Green's function.
+
+        beta : float
+            Inverse temperature :math:`\\beta`
+
+        Returns
+        -------
+
+        G_kaa : (k,m,m), ndarray
+            Green's function at the imaginary time points :math:`\\tau_k` with :math:`m \\times m` orbital indices.
+        """
 
         assert( self.xi == -1. ) # Not implemented for bosons, yet
         
@@ -127,23 +219,30 @@ class dlr(object):
 
         return G_taa
 
+    
     # -- Matsubara Frequency
 
     def get_matsubara_frequencies(self, beta):
+
         """Get Matsubara frequency grid."""
+
         zeta = (1 - self.xi)/2
         w_q = 1.j * np.pi/beta * (2*self.dlrmf + zeta)
         return w_q
 
 
     def dlr_from_matsubara(self, G_qaa, beta):
+
         """Transform the rank-3 array_like Green's function `G_qaa` from Matsbuara frequency to DLR space."""
+
         G_xaa = lu_solve((self.dlrmf2cf, self.mf2cfpiv), G_qaa.conj() / beta)
         return G_xaa
 
 
     def matsubara_from_dlr(self, G_xaa, beta):
+
         """Transform the rank-3 array_like Green's function `G_xaa` from DLR space to Matsbuara frequency."""
+
         G_qaa = beta * np.tensordot(self.T_qx, G_xaa, axes=(1, 0))
         if len(G_qaa.shape) == 3: G_qaa = np.transpose(G_qaa, axes=(0, 2, 1))
         G_qaa = G_qaa.conj()
@@ -151,7 +250,9 @@ class dlr(object):
     
     
     def eval_dlr_freq(self, G_xaa, iwn, beta):
-        """Evaluate the DLR coefficient Green's function `G_xaa` at arbibrary points in frequency space."""
+
+        """Evaluate the DLR coefficient Green's function `G_xaa` at arbibrary points `iwn` in frequency space."""
+
         w_x = self.dlrrf / beta
         G_qaa = np.einsum('x...,qx->q...', G_xaa, 1./(iwn[:, None] + w_x[None, :]))
         if len(G_qaa.shape) == 3: G_qaa = np.transpose(G_qaa, axes=(0, 2, 1))
@@ -159,27 +260,8 @@ class dlr(object):
         
         return G_qaa
     
+
     # -- Mathematical operations
-
-    def convolution_matrix(self, A_xaa, beta):
-
-        """ DLR convolution matrix with :math:`\mathcal{O}(N^2)` scaling. """
-
-        n, na, _ = A_xaa.shape
-
-        Q_xaa = np.einsum('yx,yab->xab', self.W_xx, A_xaa)
-        Q_xaa += self.k1_x[:,None,None] * A_xaa
-
-        C_xxaa = np.einsum( 'xy,yab->yxab', self.W_xx,   A_xaa)
-        C_xxaa += np.einsum('xy,yab->xyab', self.TtT_xx, A_xaa)
-        C_xxaa += np.einsum('xy,yab->xyab', np.eye(n),   Q_xaa)
-
-        C_xxaa *= beta
-
-        C_xaxa = np.moveaxis(C_xxaa, 2, 1)
-        
-        return C_xaxa
-    
 
     def convolution(self, A_xaa, B_xaa, beta):
 
@@ -203,6 +285,26 @@ class dlr(object):
         C_xaa *= beta
         
         return C_xaa
+
+    
+    def convolution_matrix(self, A_xaa, beta):
+
+        """ DLR convolution matrix with :math:`\mathcal{O}(N^2)` scaling. """
+
+        n, na, _ = A_xaa.shape
+
+        Q_xaa = np.einsum('yx,yab->xab', self.W_xx, A_xaa)
+        Q_xaa += self.k1_x[:,None,None] * A_xaa
+
+        C_xxaa = np.einsum( 'xy,yab->yxab', self.W_xx,   A_xaa)
+        C_xxaa += np.einsum('xy,yab->xyab', self.TtT_xx, A_xaa)
+        C_xxaa += np.einsum('xy,yab->xyab', np.eye(n),   Q_xaa)
+
+        C_xxaa *= beta
+
+        C_xaxa = np.moveaxis(C_xxaa, 2, 1)
+        
+        return C_xaxa
     
 
     def free_greens_function_dlr(self, H_aa, beta, S_aa=None):
