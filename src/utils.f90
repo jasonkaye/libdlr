3ebde1520b08af2de20f052421fc500555f77bfb
jasonkaye <jkaye@flatironinstitute.org>
2021-10-06 19:57:50 -0400

    Added license info to files

--- src/utils.f90
+++ src/utils.f90
@@ -1,10 +1,28 @@
+      ! -------------------------------------------------------------
       !
+      ! This file contains basic utility routines used in libdlr
       !
-      ! This file contains basic utility routines used in the code
+      ! -------------------------------------------------------------
       !
-      !
-
-
+      ! Copyright (C) 2021 The Simons Foundation
+      ! 
+      ! Author: Jason Kaye
+      ! 
+      ! -------------------------------------------------------------
+      ! 
+      ! libdlr is licensed under the Apache License, Version 2.0 (the
+      ! "License"); you may not use this file except in compliance with
+      ! the License.  You may obtain a copy of the License at
+      ! 
+      !     http://www.apache.org/licenses/LICENSE-2.0
+      ! 
+      ! Unless required by applicable law or agreed to in writing,
+      ! software distributed under the License is distributed on an "AS
+      ! IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+      ! express or implied.  See the License for the specific language
+      ! governing permissions and limitations under the License.
+      ! 
+      ! -------------------------------------------------------------
 
 
 
