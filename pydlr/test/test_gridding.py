4a1c7dec4978c5c51a00a588b5d2a28ef7c23c72
Hugo U. R. Strand <hugo.strand@gmail.com>
2022-04-11 17:53:59 +0200

    [pydlr] update tests wrt new build interface

--- pydlr/test/test_gridding.py
+++ pydlr/test/test_gridding.py
@@ -28,40 +28,10 @@ class TestBarycentricInterpolation(unittest.TestCase):
 
     def test_gridding(self, verbose=False):
 
-        lamb = 40.
-        d = dlr(lamb=lamb)
-        kmat, t, w, err = kernel_discretization(lamb, error_est=True)
-        print(f'err = {err:2.2E}')
-
-        if verbose:
-
-            import matplotlib.pyplot as plt
-            plt.figure(figsize=(6, 10))
-
-            subp = [4, 1, 1]
-
-            plt.subplot(*subp); subp[-1] += 1
-            #plt.plot(t_panel_break_pt, 0.*t_panel_break_pt, 'or')
-            plt.plot(d.t, 0.*d.t, '+')
-            plt.plot(t, 0.*t, 'x')
-
-            plt.subplot(*subp); subp[-1] += 1
-            plt.semilogy(d.t, np.abs(t - d.t), '.-')
-
-            plt.subplot(*subp); subp[-1] += 1
-            #plt.plot(w_panel_break_pt, 0.*w_panel_break_pt, 'or')
-            plt.plot(d.om, 0.*d.om, '+', alpha=0.5)
-            plt.plot(w, 0.*w, 'x')
-
-            plt.subplot(*subp); subp[-1] += 1
-            plt.semilogy(d.om, np.abs(w - d.om), '.-')
-
-            plt.show()
-
-        self.assertTrue(np.allclose(t, d.t))
-        self.assertTrue(np.allclose(w, d.om))
-        self.assertTrue(np.allclose(d.kmat, kmat))
-        self.assertTrue(err < 1e-14)
+        for lamb in [10., 40., 80., 160., 320., 640.]:
+            kmat, t, w, err = kernel_discretization(lamb, error_est=True)
+            print(f'err = {err:2.2E}')
+            self.assertTrue(err < 1e-14)
         
 
 if __name__ == '__main__':
